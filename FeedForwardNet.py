b3634f11c070d210829aad447ff3315ec03e1364
Leon-web-net <letiobi66@gmail.com>
2023-09-18 21:19:53 +0100

    Adds Updates FeedForwardNet to include Tensorboard

--- FeedForwardNet.py
+++ FeedForwardNet.py
@@ -5,13 +5,21 @@
 # Training Loop (Batch training)
 # GPU support
 
+# to use tensorboard cmd: tensorboard --logdir path/to/logs
+
 import torch
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
+from torch.utils.tensorboard import SummaryWriter
+import sys
+import torch.nn.functional as F
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+writer = SummaryWriter("path/to/logs/pr_curve")
+
+device = torch.device("cpu" if torch.cuda.is_available() else "cpu")
 # print(device)
 
 # hyper parameters
@@ -20,7 +28,7 @@ hidden_size = 100
 num_classes = 10
 num_epochs = 2
 batch_size = 100
-learning_rate = 0.001
+learning_rate = 0.004
 
 # MINST
 training_dataset = torchvision.datasets.MNIST(root="./data", train=True,
@@ -36,13 +44,16 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch
 
 # examples = iter(train_loader)
 # samples, labels = examples.next()
-for batch in train_loader:
+for batch in test_loader:
     samples, labels = batch
 print(samples.shape, labels.shape)
 
 for i in range(6):
     plt.subplot(2,3,i+1)
     plt.imshow(samples[i][0], cmap="gray")
+
+# img_grid = torchvision.utils.make_grid(samples)
+# writer.add_image("logs",img_grid)
 
 # plt.show()
 
@@ -65,8 +76,14 @@ model =  NeuralNet(input_size, hidden_size, num_classes).to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr= learning_rate)
 
+writer.add_graph(model, samples.reshape(-1,28*28))
+writer.close()
+# sys.exit()
+
 #training loop
 n_total_steps = len(train_loader)
+running_loss = 0.0
+running_correct= 0
 for epoch in range(num_epochs):
     for i,(images, labels) in enumerate(train_loader):
         # 100, 784
@@ -82,22 +99,47 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
 
+        running_loss+= loss.item()
+        _, predicted = torch.max(outputs.data, 1)
+        running_correct+= (predicted==labels).sum().item()
+
         if(i+1)%100 == 0:
             print(f"epoch {epoch+1}/{num_epochs}, step {i+1}/{n_total_steps},"
                   f"loss = {loss.item():.4f}")
+            writer.add_scalar("training loss", running_loss/100, epoch*n_total_steps+i)
+            writer.add_scalar("accuracy", running_correct/100, epoch*n_total_steps+i)
+            running_loss = 0.0
+            running_correct = 0
 # test
+labels = []
+preds = []
 with torch.no_grad():
     n_correct = 0
     n_samples = 0
-    for images, labels in test_loader:
+    for images, labels1 in test_loader:
         images = images.reshape(-1, 28 * 28).to(device)
-        labels = labels.to(device)
+        labels1 = labels1.to(device)
         outputs = model(images)
 
         # value, index (value is class label)
         _, predictions = torch.max(outputs,1)
-        n_samples += labels.shape[0]
-        n_correct += (predictions==labels).sum().item()
+        n_samples += labels1.shape[0]
+        n_correct += (predictions==labels1).sum().item()
+
+        class_predictions = [F.softmax(output, dim=0) for output in outputs]
+
+        preds.append(class_predictions)
+        labels.append(predicted)
+
+    preds = torch.cat([torch.stack(batch) for batch in preds])
+    labels = torch.cat(labels)
 
     acc  = 100.0 * n_correct/n_samples
     print(f"accuracy = {acc}%")
+
+    classes = range(10)
+    for i in classes:
+        labels_i = labels == i
+        preds_i = preds[:,i]
+        writer.add_pr_curve(str(i), labels_i, preds_i, global_step=0)
+        writer.close()
